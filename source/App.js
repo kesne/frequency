fa012eaf9ed80eed56e422ba621d1f02a44bc4f8
kesne <jordangens@gmail.com>
2012-07-08 19:40:28 -0700

    Added touchtap button to increase performance on mobile devices. Added scrollers. Tick version.

--- source/App.js
+++ source/App.js
@@ -34,18 +34,18 @@ enyo.kind({
 		{kind: "onyx.Toolbar", content: "Frequency Study"},
 		{kind: "Scroller", fit: true, components: [
 			{style: "padding: 20px", components: [
-				{kind: "onyx.TouchButton", content: "Employees", onclick: "employees", style: "width: 100%"},
+				{kind: "onyx.TouchButton", content: "Employees", ontouchtap: "employees", style: "width: 100%"},
 				{tag: "br"},{tag: "br"},
-				{kind: "onyx.TouchButton", content: "Activities", onTouchTap: "buttonPress", style: "width: 100%"},
+				{kind: "onyx.TouchButton", content: "Activities", onclick: "employees", style: "width: 100%"},
 				{tag: "br"},{tag: "br"},
-				{kind: "onyx.TouchButton", content: "Manage Studies", onTouchTap: "buttonPress", style: "width: 100%"},
+				{kind: "onyx.TouchButton", content: "Manage Studies", ontouchtap: "buttonPress", style: "width: 100%"},
 				{tag: "br"},{tag: "br"},{tag: "br"},{tag: "br"},
-				{kind: "onyx.TouchButton", content: "Perform a Study", onTouchTap: "buttonPress", style: "width: 100%; background-color: #1E6B00; color: white;"}
+				{kind: "onyx.TouchButton", content: "Perform a Study", ontouchtap: "buttonPress", style: "width: 100%; background-color: #1E6B00; color: white;"}
 			]}
 		]}
 	],
 	employees: function(){
-		this.bubble("onView", {name: "employees"})
+		this.bubble("onView", {name: "employees"});
 	}
 });
 
@@ -53,33 +53,17 @@ enyo.kind({
 	name: "freq.employees",
 	kind: "FittableRows",
 	components: [
-		{kind: "onyx.Toolbar", content: "Employees"},
-		{kind: "Scroller", fit: true, touch: false, components: [
-			{kind: "onyx.TouchButton", content: "Employees", onTouchTap: "employees", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Activities", onTouchTap: "buttonPress", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Manage Studies", onTouchTap: "buttonPress", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Perform a Study", onTouchTap: "buttonPress", style: "width: 100%; background-color: #1E6B00; color: white;"},
-			{kind: "onyx.TouchButton", content: "Employees", onTouchTap: "employees", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Activities", onTouchTap: "buttonPress", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Manage Studies", onTouchTap: "buttonPress", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Perform a Study", onTouchTap: "buttonPress", style: "width: 100%; background-color: #1E6B00; color: white;"},
-			{kind: "onyx.TouchButton", content: "Employees", onTouchTap: "employees", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Activities", onTouchTap: "buttonPress", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Manage Studies", onTouchTap: "buttonPress", style: "width: 100%"},
-			{tag: "br"},{tag: "br"},{tag: "br"},{tag: "br"},
-			{kind: "onyx.TouchButton", content: "Perform a Study", onTouchTap: "buttonPress", style: "width: 100%; background-color: #1E6B00; color: white;"}
+		{kind: "onyx.Toolbar", layoutKind: "FittableColumnsLayout", components: [
+			{content: "Employees"},
+			{fit: true},
+			{kind: "onyx.TouchButton", content: "Back", ontouchtap: "back"}
+		]},
+		{kind: "Scroller", fit: true, components: [
+			{content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec massa quam, sed porttitor libero. Nunc scelerisque pulvinar varius. Aliquam in neque vel nibh tincidunt volutpat non nec dui. Pellentesque sit amet volutpat libero. Ut sapien odio, lacinia eget volutpat a, sollicitudin a orci. Nunc lacinia scelerisque lorem ut cursus. Nunc malesuada volutpat imperdiet. In lobortis quam vel ligula feugiat sit amet sollicitudin elit varius. Maecenas orci eros, volutpat sed sodales et, hendrerit nec ante. Aliquam fermentum sem in lorem sodales nec tempus lectus interdum. Cras elementum consequat scelerisque. Vivamus non bibendum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur luctus tellus vitae sem pharetra bibendum. Quisque ultricies, leo in scelerisque adipiscing, odio urna mollis elit, eget volutpat magna ipsum ut magna. Curabitur quis aliquet urna."}
 		]}
 	],
-	employees: function(){
-		this.bubble("onView", {name: "employees"})
+	back: function(){
+		this.bubble("onView", {name: "home"})
 	}
 });
 
@@ -87,24 +71,37 @@ enyo.kind({
 	name: "onyx.TouchButton",
 	kind: "onyx.Button",
 	events: {
-		onTouchTap: ""
+		ontouchtap: ""
 	},
 	handlers: {
 		ontouchstart: "handleTouchstart",
 		ontouchmove: "handleTouchmove",
-		ontouchend: "handleTouchend"
+		ontouchend: "handleTouchend",
+		
+		//Desktop catch:
+		ontap: "desktopCatch"
 	},
+	//Prevent drag interference
 	moving: false,
 	
+	//Bubble for non-touch devices:
+	desktopCatch: function(inSender, inEvent){
+		if(!enyo.platform.touch){
+			this.bubble("ontouchtap", inEvent);
+		}
+	},
+	
+	//Touch Button Logic:
 	handleTouchstart: function(){
 		this.moving = false;
 	},
 	handleTouchmove: function(){
 		this.moving = true;
 	},
-	handleTouchend: function(){
+	handleTouchend: function(inSender, inEvent){
 		if(this.moving !== true){
-			this.bubble("onTouchTap");
+			//Bubble up the ontouchtap event:
+			this.bubble("ontouchtap", inEvent);
 		}
 	}
 });
