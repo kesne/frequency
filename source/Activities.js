da8033934a3754611b4185dd02ae996dd45c32a4
kesne <jordangens@gmail.com>
2012-07-11 11:36:02 -0700

    Updated with panels.

--- source/Activities.js
+++ source/Activities.js
@@ -3,18 +3,21 @@ enyo.kind({
 	kind: "FittableRows",
 	activities: {},
 	components: [
-		{kind: "onyx.Toolbar", layoutKind: "FittableColumnsLayout", components: [
-			{content: "Activity Categories"},
+		{kind: "onyx.Toolbar", name: "Header", layoutKind: "FittableColumnsLayout", components: [
+			{content: "Activity Categories", name: "HeaderContent"},
 			{fit: true},
 			{kind: "onyx.TouchButton", content: "Back", ontouchtap: "back"}
 		]},
-		{kind: "List", name: "act", fit: true, count: 0, onSetupItem: "setupItem", components: [
-	        {kind: "onyx.SwipeableItem", onDelete: "deleteRow", components: [
-				{name: "item", classes: "item enyo-border-box", components: [
-					{name: "name"}
+		{kind: "Panels", name: "panels", fit: true, draggable: false, animate: false, components: [
+			{kind: "List", name: "act", count: 0, onSetupItem: "setupItem", components: [
+		        {kind: "onyx.SwipeableItem", onDelete: "deleteRow", ontap: "selectRow", components: [
+					{name: "item", classes: "item enyo-border-box", components: [
+						{name: "name"}
+					]}
 				]}
-			]}
-	    ]},
+		    ]},
+		    {content: "Activities List!"}
+		]},
 		{kind: "onyx.Toolbar", name: "footer", layoutKind: "FittableColumnsLayout", components: [
 			{fit: true},
 			{kind: "onyx.IconButton", src: "assets/menu-icon-add.png", ontap: "addCategory", classes: "onyx-icon-right", style: "height: 32px; width: 150px;"}
@@ -46,22 +49,36 @@ enyo.kind({
 		]},
 		{kind: "onyx.Popup", name: "deleteConfirm", centered: true, autoDismiss: false, modal: true, scrim: true, floating: true, style: "width: 320px;", components: [
 			{content: "Delete Category", style: "font-size: 1.2em; font-weight: bold; padding: 5px;"},
-			{content: "", name: "deleteConfirmContent", style: "padding: 5px;"},
+			{content: "", name: "deleteConfirmContent", style: "padding: 5px;", allowHtml: true},
 			{content: "Please note that all of the activities in the category will also be deleted.", style: "padding: 5px; margin-bottom: 10px;"},
 			{components: [
-				{kind: "onyx.Button", content: "Cancel", ontap: "closeError", classes: "onyx-button-dark", style: "width: 150px; height: 38px; float: left;"},
-				{kind: "onyx.Button", content: "Delete", ontap: "closeError", classes: "onyx-button-dark", style: "width: 150px; height: 38px; float: right;"}
+				{kind: "onyx.Button", content: "Cancel", ontap: "cancelDelete", classes: "onyx-button-dark", style: "width: 150px; height: 38px; float: left;"},
+				{kind: "onyx.Button", content: "Delete", ontap: "deleteCat", classes: "onyx-button-dark", style: "width: 150px; height: 38px; float: right;"}
 			]}
 		]}
 	],
 	
+	/*
+	 * Delete Functions:
+	 */
 	deleteRow: function(inSender, inEvent){
-		var name = this.activitiesArray[inEvent.index];
-		this.$.deleteConfirmContent.setContent('Are you sure you want to delete the category "' + name + '"?');
+		this.deleting = this.activitiesArray[inEvent.index];
+		this.$.deleteConfirmContent.setContent('Are you sure you want to delete the category "<strong>' + this.deleting + '</strong>"?');
 		this.$.deleteConfirm.show();
-		console.log(inEvent.index); //<-- index of the array!
+	},
+	cancelDelete: function(){
+		this.$.deleteConfirm.hide();
+	},
+	deleteCat: function(){
+		delete this.activities[this.deleting];
+		this.saveActivities();
+		this.gotActivities();
+		this.$.deleteConfirm.hide();
 	},
 	
+	/*
+	 * Add Functions:
+	 */
 	addCategory: function(){
 		this.$.addCategory.show();
 	},
@@ -86,11 +103,15 @@ enyo.kind({
 		}
 	},
 	checkEnter: function(inSender, inEvent){
+		//Add if enter is pressed in the input box:
 		if(inEvent.keyCode === 13){
 			this.confirmAdd();
 		}
 	},
 	
+	/*
+	 * Utility functions to show and hide the error dialog:
+	 */
 	showError: function(){
 		this.$.errorPopup.show();
 	},
@@ -112,6 +133,7 @@ enyo.kind({
 		}));
 	},
 	
+	//Sets the data, if it is provided, and displays the list:
 	gotActivities: function(act){
 		if(act){
 			this.activities = act;
@@ -125,6 +147,7 @@ enyo.kind({
 		this.$.act.reset();
 	},
 	
+	//Sets up a list item:
 	setupItem: function(inSender, inEvent) {
 		// given some available data.
 	    var data = this.activitiesArray[inEvent.index];
@@ -132,7 +155,20 @@ enyo.kind({
 	    this.$.name.setContent(data);
 	},
 	
+	//Select a category:
+	selectRow: function(){
+		this.$.panels.setIndex(1);
+		this.$.HeaderContent.setContent("Activities");
+		this.$.Header.reflow();
+	},
+	
 	back: function(){
-		this.bubble("onView", {name: "home"})
+		if(this.$.panels.getIndex() === 0){
+			this.bubble("onView", {name: "home"});
+		}else{
+			this.$.panels.setIndex(0);
+			this.$.HeaderContent.setContent("Activity Categories");
+			this.$.Header.reflow();
+		}
 	}
 });
